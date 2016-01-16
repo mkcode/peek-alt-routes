a7e49d9d9989130c9de925b5bf23f96628b3fe04
Chris Ewald <chrisewald@gmail.com>
2016-01-15 20:38:09 -0500

    inject request extensions via initializer

--- lib/peek-alt-routes/railtie.rb
+++ lib/peek-alt-routes/railtie.rb
@@ -1,6 +1,14 @@
+require 'peek/alt-routes/request_extensions'
+
 module Peek
   module AltRoutes
     class Railtie < ::Rails::Engine
+      initializer 'peek.alt_routes.include_request_extensions' do
+        ActiveSupport.on_load(:action_controller) do
+          ActionDispatch::Request.send(:include, Peek::AltRoutes::RequestExtensions)
+          delegate :peek_alt_routes?, to: :request
+        end
+      end
     end
   end
 end
